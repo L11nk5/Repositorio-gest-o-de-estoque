cfc315283c1e4b03db7b7b45e73127ad985a4511
L11nk5 <gabrielmastrangelo12@gmail.com>
2026-03-25 22:15:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,22 @@
-const CACHE = "estoque-v2";
+const CACHE = "estoque-v3";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll([
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
         "./",
         "./index.html",
         "./logo.png",
         "./manifest.json"
-      ])
-    )
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});const CACHE = "estoque-v2";
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll(["./", "./index.html", "./logo.png"])
-    )
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
