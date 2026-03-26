9db6bd68cf47579d97e7f1a7b9af9255585374aa
L11nk5 <gabrielmastrangelo12@gmail.com>
2026-03-25 22:05:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,23 @@
 const CACHE = "estoque-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        "./",
+        "./index.html",
+        "./logo.png",
+        "./manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});const CACHE = "estoque-v2";
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE).then(c =>
